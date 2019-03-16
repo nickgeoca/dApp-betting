4fa21a6d211198bc1ce2ec31c6f53543c36ac6ab
Nick Geoca <nickgeoca@gmail.com>
2019-03-16 08:38:44 -0500

    Added web code

--- client/src/App.js
+++ client/src/App.js
@@ -54,22 +54,27 @@ class App extends Component {
   getValues = async () => {
     const { accounts, contract } = this.state;
 
-    let outcomePrice = (await contract.methods.outcomePrice().call()).toString(10);
-    outcomePrice = outcomePrice === '0'? 'outcome not set': outcomePrice;
-    this.setState({ outcomePrice: outcomePrice });
-    console.log(await contract.methods.strikePrice().call());
     const strikePrice = ((await contract.methods.strikePrice().call()) / 1000).toString(10);
     this.setState({ strikePrice: strikePrice });
 
     let userBet = (await contract.methods.userBet(accounts[0]).call()).toString(10);
     userBet = userBet === '0'? "Have not betted"
-            : userBet === '1'? "You bet .01 ether that the outcome price will go low"
-            : userBet === '2'? "You bet .01 ether that the outcome price will go high"
+            : userBet === '1'? "You bet .01 ether that the final price will be above $" + strikePrice
+            : userBet === '2'? "You bet .01 ether that the final price will be above $" + strikePrice
             :                  '?';
     this.setState({ userBet: userBet });
 
-    console.log(contract);
     this.setState({ contractAddress: contract._address });
+
+    let didWin = await contract.methods.didUserWin(accounts[0]).call();
+    let outcomePrice = (await contract.methods.outcomePrice().call()).toString(10);
+    didWin = didWin? 'You have won!': (outcomePrice==='0'? 'Waiting for outcome...': 'Better luck nextime!');
+    console.log(await contract.methods.userCollectedWinings(accounts[0]).call());
+    didWin = (await contract.methods.userCollectedWinings(accounts[0]).call())? 'Winnings collected!' : didWin;
+    this.setState({ userDidWin: didWin})
+
+    if (outcomePrice !== '0') this.setState({ finalPrice: 'Final Price of Oil $' + outcomePrice / 1000})
+    else this.setState({ finalPrice: ''})
   }
 
   render() {
@@ -79,24 +84,29 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Toy Gambling dApp - Place Your Wager!</h1>
-        <h4>Is oil going to be above {this.state.strikePrice} by the end of the presentation? </h4>
+        <h4>Is oil going to be above ${this.state.strikePrice} by the end of the presentation? </h4>
         <h5><a href="https://www.theice.com/products/219/Brent-Crude-Futures/data?marketId=222470">See current price here</a></h5>
-
+        <hr/>
         <button onClick={this.betAbove}>
-        Bet Price Goes Up
+        Bet Final Price is Higher
         </button>
         <button onClick={this.betBelow}>
-        Bet Price Goes Down
-        </button>
-        <button onClick={this.collectWinnings}>
-        Collect Winnings
+        Bet Final Price is Lower
         </button>
       
         <div>{this.state.userBet}</div>
-        <div>Strike price: {this.state.strikePrice}</div>
-
-      <a href={"https://rinkeby.etherscan.io/address/"+this.state.contractAddress}>Betting Smart Contract</a>
-      <a href={"https://rinkeby.etherscan.io/address/"+this.state.accounts[0]}>Your address</a>
+        <hr/>
+        <ul><h2>Ethereum Testnet Explorer</h2>
+        <li><a href={"https://rinkeby.etherscan.io/address/"+this.state.contractAddress}>Betting Smart Contract</a></li>
+        <li><a href={"https://rinkeby.etherscan.io/address/"+this.state.accounts[0]}>Your Address</a></li>
+        </ul>
+        <hr/>
+        
+      {this.state.userDidWin}<br/>{this.state.finalPrice}
+        <button onClick={this.collectWinnings}>
+        Collect Winnings
+        </button>
+       
       </div>
     );
   }
